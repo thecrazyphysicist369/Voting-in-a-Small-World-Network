edf083fa342f522d46bb2e274d781af3e1c9182e
Shaun Ray <shaun.ray.3000@gmail.com>
2019-03-03 13:58:20 +0530

    the network file and the display file
    the network file houses the graph and the details and the display file showgraph displays the generated graph for ease of use

--- network.py
+++ network.py
@@ -1,1 +1,55 @@
-'''this file will be used to build and maintain the network of the voters'''
+import random
+vertex = {'index': 0,
+          'endurance': 0,
+          'influence': 0,
+          'connected': [],
+          }
+
+#vertex generator
+def gen_vertex(n):
+    ver_list=[]
+    c=0
+    for i in range(n):
+        vertex = {'index': c,
+                  'endurance': random.randrange(-10, 10),
+                  'influence': random.randint(0, 10),
+                  'connected': [],
+                  }
+        ver_list.append(vertex)
+
+        c+=1
+    return ver_list
+
+
+#matrix generator
+def mat_gen(n):
+    mat = []
+    c = 0
+    for i in range(n):
+        mat.append(random.sample(range(n), n))
+        for j in range(i):
+            mat[i][j] = mat[j][i] = random.randint(0, 1)
+        mat[i][i] = 0
+    return mat
+
+
+
+
+
+#the graph generator
+def gen_graph(n):
+
+    mat=mat_gen(n)
+    #print(mat[:][:])
+    verlist = gen_vertex(n)
+    for i in range(n):
+        conlist=[]
+        for j in range(n):
+            if mat[i][j] == 1:
+                conlist.append(j)
+                verlist[i]['connected']=conlist
+        #print(conlist)
+    return verlist
+
+print(len(gen_graph(10)))
+
